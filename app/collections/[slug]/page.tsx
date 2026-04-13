cb9d77c0e28a5b1295ee16edbb91f22b967af15c
sahil1815 <fire28307@gmail.com>
2026-04-13 12:01:25 +0600

    Changing money sign

--- app/collections/[slug]/page.tsx
+++ app/collections/[slug]/page.tsx
@@ -80,7 +80,7 @@ export default async function CollectionPage(
                     )}
                   </div>
                   <h3 className="text-sm font-medium text-[#1A1A1A] tracking-wide">{product.name}</h3>
-                  <p className="text-sm text-[#1A1A1A]/70 mt-1">৳{(product.basePrice || 0).toFixed(2)}</p>
+                  <p className="text-[#1A1A1A]/70 mt-1"><span className="text-md">৳</span><span className="text-sm">{(product.basePrice || 0).toFixed(2)}</span></p>
                 </Link>
               );
             })
