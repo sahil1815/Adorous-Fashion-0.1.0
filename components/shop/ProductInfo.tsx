f1df920ae4752d2422ec60a1da47365ca05f4f74
sahil1815 <fire28307@gmail.com>
2026-04-15 14:06:14 +0600

    Changed the productinfo.tsx file

--- components/shop/ProductInfo.tsx
+++ components/shop/ProductInfo.tsx
@@ -65,14 +65,12 @@ export interface ProductInfoProps {
 // Helpers
 // ---------------------------------------------------------------------------
 
-function formatPrice(amount: number, currency = "USD") {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency,
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 2,
-  }).format(amount);
+function formatPrice(amount: number) {
+  return "৳" + amount.toLocaleString("en-IN", {
+    maximumFractionDigits: 0,
+  });
 }
+
 
 function getSavings(price: number, compare: number) {
   return Math.round(((compare - price) / compare) * 100);
